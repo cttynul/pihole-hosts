b8de7a154d39fe99d54d782b4e15403ce380c566
cttynul <cttynul@outlook.com>
2020-12-12 12:11:38 +0100

    + support to all sb hosts file

---
+++ sb.php
@@ -0,0 +1,27 @@
+<?php
+    if(isset($_GET['m'])){
+      $url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/" . $_GET['m'] . "/hosts";
+    else{
+      $url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts";
+    }
+    
+    function get_contents($url, $u = false, $c = null, $o = null) {
+      $headers = get_headers($url);
+      $status = substr($headers[0], 9, 3);
+      if ($status == '200') {
+          return file_get_contents($url, $u, $c, $o);
+      }
+      return false;
+    }
+      
+    $hosts_file = get_contents($url);
+
+    if ( $hosts_file === false ){
+      echo "Can't connect to " . $url .  " :(";
+    }
+    else{
+      $pattern = '/ *0\.0\.0\.0 */';
+      $replacement = "";
+      echo preg_replace($pattern, $replacement, $hosts_file);
+    }
+?>
