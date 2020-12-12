df95b6286fa7a90a3927ceb05fae579dd8f965ea
cttynul <cttynul@outlook.com>
2020-12-12 11:51:00 +0100

    :shipit:

--- nsa.php
+++ nsa.php
@@ -1,6 +1,6 @@
 <?php
   $hosts_file = file_get_contents("https://raw.githubusercontent.com/cttynul/NSABlocklist/master/HOSTS");
-  $pattern = '/ *0\.0\.0\.0 */gm';
+  $pattern = '/ *0\.0\.0\.0 */';
   $replacement = "";
   echo preg_replace($pattern, $replacement, $hosts_file);
 ?>
