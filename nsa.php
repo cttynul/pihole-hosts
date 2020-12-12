0ace9f72f05e43dee1c10e109738e380d3fd5699
cttynul <cttynul@outlook.com>
2020-12-12 11:49:32 +0100

    i'm a stupid py dev

--- nsa.php
+++ nsa.php
@@ -1,5 +1,5 @@
 <?php
-  $hosts_file = file_get_contents("https://raw.githubusercontent.com/cttynul/NSABlocklist/master/HOSTS")
+  $hosts_file = file_get_contents("https://raw.githubusercontent.com/cttynul/NSABlocklist/master/HOSTS");
   $pattern = '/ *0\.0\.0\.0 */gm';
   $replacement = "";
   echo preg_replace($pattern, $replacement, $hosts_file);
